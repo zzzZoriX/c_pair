dfcd36edbd2733ed859fe4540ed40cf50aa57e91
anyway <zorix228337@gmail.com>
2025-11-03 21:51:27 +0300

    added define static pair function

--- cpair.h
+++ cpair.h
@@ -9,7 +9,7 @@ typedef struct {                                                            \
     K right;                                                                \
 } pair_##T##_##K;                                                           \
                                                                             \
-pair_##T##_##K* new_pair(                                                   \
+pair_##T##_##K* new_pair_##T##_##K(                                         \
     T left,                                                                 \
     K right                                                                 \
 ){                                                                          \
@@ -20,9 +20,31 @@ pair_##T##_##K* new_pair(                                                   \
     return np;                                                              \
 }                                                                           \
                                                                             \
-void delete_pair(pair_##T##_##K* pair){                                     \
+void delete_pair_##T##_##K(pair_##T##_##K* pair){                           \
     free(pair);                                                             \
 }
+
+#define DEFINE_STATIC_PAIR(T, K)                                            \
+typedef struct {                                                            \
+    T left;                                                                 \
+    K right;                                                                \
+} pair_##T##_##K;                                                           \
+                                                                            \
+static pair_##T##_##K* new_pair_##T##_##K(                                  \
+    T left,                                                                 \
+    K right                                                                 \
+){                                                                          \
+    pair_##T##_##K* np = (pair_##T##_##K*)malloc(sizeof(pair_##T##_##K));   \
+    np->left = left;                                                        \
+    np->right = right;                                                      \
+                                                                            \
+    return np;                                                              \
+}                                                                           \
+                                                                            \
+static void delete_pair_##T##_##K(pair_##T##_##K* pair){                    \
+    free(pair);                                                             \
+}
+
 
 
 #endif // CPAIR_H
